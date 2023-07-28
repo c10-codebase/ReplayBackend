104ff908651d83a5f0c438d56477812f69617fff
adhiyaman <adhiyaman@artifintel.in>
2023-07-28 12:33:18 +0530

    ss

--- config/db.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import connect
-
-def db_connection():
-    data = connect('medical', host='127.0.0.1', port=27017)
-    if data:
-        return "success"
-    else:
-        return "failure"
--- models.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import Document, StringField ,IntField
-
-class User_data(Document):
-    name = StringField(required=True)
-    email = StringField(required=True, unique=True)
-    password = StringField(required=True)
-    mobile = IntField(required=True)
